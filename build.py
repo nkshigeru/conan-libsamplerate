fcf0208f0685055f35b2ad9b232b362114f7380e
nkshigeru <1712694+nkshigeru@users.noreply.github.com>
2019-04-10 17:06:16 +0900

    Remove shared option on Visual Studio

--- build.py
+++ build.py
@@ -6,4 +6,8 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder(pure_c=True)
+    builder.remove_build_if(lambda build: build.settings["compiler"] == "Visual Studio" and build.options["libsamplerate:shared"] == False)
+    for build in builder.items:
+        if build.settings["compiler"] == "Visual Studio":
+            del build.options["libsamplerate:shared"]
     builder.run()
